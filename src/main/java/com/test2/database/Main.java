beea14417ede8850697e1231661ce93d5c070cee
Ikar336 <karunos.2015@mail.ru>
2025-07-16 02:10:28 +0700

    Обновил зависимости иСоздал

---
+++ src/main/java/com/test2/database/Main.java
@@ -0,0 +1,22 @@
+package com.test2.database;
+
+import java.sql.Connection;
+import java.sql.Driver;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class Main {
+    private final static String URL = "jdbc:mysql://localhost:3306/test1";
+    private final static String USERNAME = "root";
+    private final static String PASSWORD = "root";
+
+    public static void main(String[] args) {
+        try (Connection connection = DriverManager.getConnection(URL, USERNAME, PASSWORD)) {
+            if (!connection.isClosed()) {
+                System.out.println("We are connected!");
+            }
+        } catch (SQLException e) {
+            System.out.println("there is no connection... Exception!");
+        }
+    }
+}
